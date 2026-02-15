77a0ef469b23ec125059564e436a6320c0d04596
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-02-15 11:24:52 -0500

    Added client flying

---
+++ client-flying/src/main/java/com/example/clientflying/ClientFlyingMod.java
@@ -0,0 +1,47 @@
+package com.example.clientflying;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.world.GameMode;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+
+public class ClientFlyingMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		System.out.println("[ClientFlying] Client initialized");
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (client.player != null && client.interactionManager != null) {
+				GameMode gameMode = client.interactionManager.getCurrentGameMode();
+				boolean isOkMode = gameMode == GameMode.SURVIVAL || gameMode == GameMode.ADVENTURE;
+				ItemStack chestStack = client.player.getEquippedStack(EquipmentSlot.CHEST);
+				boolean isElytra = chestStack.isOf(Items.ELYTRA);
+				if (isOkMode) {
+					client.player.getAbilities().allowFlying = !isElytra;
+					client.player.sendAbilitiesUpdate();
+				}
+			}
+			ClientPlayNetworkHandler net = client.getNetworkHandler();
+			if (net != null){
+				double x = client.player.getX();
+				double y = client.player.getY();
+				double z = client.player.getZ();
+
+				float yaw = client.player.getYaw();
+				float pitch = client.player.getPitch();
+
+				net.sendPacket(new PlayerMoveC2SPacket.Full(
+					x, y, z,
+					yaw, pitch,
+					true,
+					client.player.horizontalCollision
+				));
+			}
+		});
+	}
+}
