d423383b14cb9f57563219d8b1edd47712f00bf4
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-07-15 16:34:03 -0400

    Remove useless file

--- better-step/src/main/java/com/example/betterstep/BetterStepMod.java
+++
@@ -1,9 +0,0 @@
-package com.example.betterstep;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class BetterStepMod implements ClientModInitializer {
-
-    @Override
-    public void onInitializeClient() {}
-}
