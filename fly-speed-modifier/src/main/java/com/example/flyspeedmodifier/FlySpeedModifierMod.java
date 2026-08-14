c42fe1d3cdbebafc815ccb0601051eac86610602
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-08-14 10:54:23 -0400

    Update FlySpeedModifierMod.java

--- fly-speed-modifier/src/main/java/com/example/flyspeedmodifier/FlySpeedModifierMod.java
+++ fly-speed-modifier/src/main/java/com/example/flyspeedmodifier/FlySpeedModifierMod.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
 
